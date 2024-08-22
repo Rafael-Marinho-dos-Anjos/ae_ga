9b6901117b4cf8553a1a887d0dfb60e4e754121e
constantino1 <Constantinoneto14@gmail.com>
2024-08-22 12:00:39 -0300

    Validation dataloader batch sizer changed to 16

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -8,5 +8,5 @@ from dataset.ds import train_ds, test_val_ds
 train_dl = DataLoader(train_ds, batch_size=16, shuffle=True)
 
 test_ds, val_ds = random_split(test_val_ds, (0.5, 0.5))
+val_dl = DataLoader(val_ds, batch_size=16)
 test_dl = DataLoader(test_ds)
-val_dl = DataLoader(val_ds)
